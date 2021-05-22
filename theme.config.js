62e0506cdf55e40f921748fc86e0a060a45e1268
Matheus Frata <git@mfrata.com>
2021-05-22 10:24:55 -0300

    fixup! fixup! Change posts to blog

--- theme.config.js
+++ theme.config.js
@@ -1,10 +1,9 @@
 const YEAR = new Date().getFullYear()
 
 export default {
-  titleSuffix: ' – MFrata',
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © mfrata
+      <time>{YEAR}</time> © MFrata
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
